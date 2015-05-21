204828c3aca9a3d6ded5977cb28a1d6bb961338d
dkdelosreyes <diannekatherine@Dianne-Katherine-Delos-Reyes.local>
2015-05-21 12:22:37 +0800

    irdk

--- app/routes.php
+++ app/routes.php
@@ -12,14 +12,14 @@
 */
 
 Route::get('/', 'DefaultController@index');
-<<<<<<< HEAD
+
 Route::get('subscribe', 'DefaultController@subscribe');
 Route::post('subscribe/doSubscribe', 'DefaultController@doSubscribe');
-=======
+
 
 Route::resource('products', 'ProductsController');
 Route::resource('categories', 'CategoriesController');
 Route::resource('customers', 'CustomersController');
 Route::resource('orders', 'OrdersController');
 Route::resource('subscriptions', 'SubscriptionsController');
->>>>>>> 498238c2cb51e479c13d96a39af6a8a4a5d3c1a8
+
